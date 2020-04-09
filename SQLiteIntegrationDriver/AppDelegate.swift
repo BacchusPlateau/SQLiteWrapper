f9b893cd1f0eeadc5ebd9f57388e0d6b0021bb59
Bret Williams <straightener@hotmail.com>
2020-04-09 15:17:19 -0500

    Initial Commit

---
+++ SQLiteIntegrationDriver/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  SQLiteIntegrationDriver
+//
+//  Created by Bret Williams on 4/9/20.
+//  Copyright © 2020 Bret Williams. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
